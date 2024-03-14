ae4114590c1f9d681b1011993f6885e940807023
hickecha <hickecha@sheridancollege.ca>
2024-03-14 11:55:05 -0400

    Task 4b started: Setup Canvas and text stuff.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,18 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI currentSelection;
+    public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
+
+    private void Start()
+    {
+        Instance = this;
+    }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
+            //currentSelection.text = "Selected: Nothing";
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        //currentSelection.text = villager.name;
     }
     
 }
