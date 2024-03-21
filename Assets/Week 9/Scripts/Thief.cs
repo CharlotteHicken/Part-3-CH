c8bfe20443eead6ce924c834ee8b5ec3f5fb4b83
hickecha <hickecha@sheridancollege.ca>
2024-03-21 09:58:46 -0400

    Task 1 complete: Dashing with a coroutine

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Thief : Villager
@@ -7,14 +8,33 @@ public class Thief : Villager
     public GameObject daggerPrefab;
     public Transform leftSpawnPoint;
     public Transform rightSpawnPoint;
+    Coroutine dashing;
 
     protected override void Attack()
     {
-        transform.position = destination;
+        if (dashing != null)
+        {
+            StopCoroutine(dashing);
+        }
+        dashing = StartCoroutine(Dash());
+    }
+
+    IEnumerator Dash()
+    {
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        speed = 7;
+        while (speed > 3)
+        {
+            yield return null;
+        }
         base.Attack();
+        yield return new WaitForSeconds(0.1f);
         Instantiate(daggerPrefab, leftSpawnPoint.position, leftSpawnPoint.rotation);
+        yield return new WaitForSeconds(0.15f);
         Instantiate(daggerPrefab, rightSpawnPoint.position, rightSpawnPoint.rotation);
     }
+  
+
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
