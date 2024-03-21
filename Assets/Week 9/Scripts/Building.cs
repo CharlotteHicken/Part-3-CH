51bdf66a6bc9e54b1097e594b38b155fb79cde26
DESKTOP-9E3IQ19\Valued Customer <hickecha@shernet.sheridancollege.ca>
2024-03-21 15:06:21 -0400

    Task 3 complete: created a building that instatiates and has a scaling up effect handled by a coroutine

--- Assets/Week 9/Scripts/Building.cs
+++ Assets/Week 9/Scripts/Building.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    public GameObject fullWagon;
-    public float speed = 1f;
-    float wheeltime = 0f;
+    public GameObject spawnpoint;
+    public GameObject wagonPrefab;
+    public GameObject[] buildingPieces; 
+    public float scale = 2f;
+    public float duration = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +20,26 @@ public class Building : MonoBehaviour
 
     IEnumerator Build()
     {
-        Instantiate(fullWagon);
-        //GameObject wheel = new GameObject(fullWagon.GetComponentInChildren<Transform>());    
+        GameObject wagon = Instantiate(wagonPrefab, spawnpoint.transform);
         yield return null;
+        foreach (GameObject buildingPiece in buildingPieces)
+        {
+            GameObject piece = wagon.transform.Find(buildingPiece.name).gameObject;
+            yield return StartCoroutine(ScalePiece(piece.transform));
+        }
+    }
+
+    IEnumerator ScalePiece(Transform pieceScale)
+    {
+        float time = 0f;
         
-        
+        while (time < duration)
+        {
+            float t = time / duration;
+            pieceScale.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, t);
+            time += Time.deltaTime;
+            yield return null;
+        }
     }
 
 }
